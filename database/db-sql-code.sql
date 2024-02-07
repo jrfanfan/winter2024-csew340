bacb49071d83bc5e43970bc0f0a6924b2eeed85f
Fanfan <jrfanfan@yahoo.fr>
2024-02-07 10:40:46 -0500

    update

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -241,3 +241,15 @@ VALUES   (
     'White',
     5
   );
+
+
+-- Update for table `inventory`
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'the small interiors', 'a huge interior')
+WHERE inv_id = 10;
+
+-- UPDATE for table `inventory`
+UPDATE public.inventory
+SET
+ inv_image = REPLACE(inv_image, 'images/', 'images/vehicles/'),
+ inv_thumbnail = REPLACE(inv_thumbnail, 'images/', 'images/vehicles/');

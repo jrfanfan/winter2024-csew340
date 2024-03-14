543c9609d83c497a2df7cff6e4ba4e2c91c1fed4
Fanfan <jrfanfan@yahoo.fr>
2024-03-14 19:07:10 -0400

    update

--- controllers/invController.js
+++ controllers/invController.js
@@ -42,9 +42,11 @@ invCont.buildByClassificationDetail = async function (req, res) {
  * ************************** */
 invCont.buildByManagement = async function (req, res) {
   let nav = await utilities.getNav()
+  const classificationSelect = await utilities.builAddInvetory()
   res.render("./inventory/management", {
     title: "Management",
     nav,
+    classificationSelect,
     errors: null,
   })
 }
@@ -165,5 +167,17 @@ invCont.registerNewInventory = async function(req, res) {
 
 }
 
+/* ***************************
+ *  Return Inventory by Classification As JSON
+ * ************************** */
+invCont.getInventoryJSON = async (req, res, next) => {
+  const classification_id = parseInt(req.params.classification_id)
+  const invData = await invModel.getInventoryByClassificationId(classification_id)
+  if (invData[0].inv_id) {
+    return res.json(invData)
+  } else {
+    next(new Error("No data returned"))
+  }
+}
 
 module.exports = invCont
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -30,4 +30,5 @@ router.post(
   utilities.handleErrors(invController.registerNewInventory)
 )
 
+router.get("/getInventory/:classification_id", utilities.handleErrors(invController.getInventoryJSON))
 module.exports = router;
